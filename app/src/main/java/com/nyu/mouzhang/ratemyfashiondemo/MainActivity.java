ebf92d43d1ed1091c7061dece378849516b80f51
Kai Mou <kaimou1357@gmail.com>
2016-04-07 19:02:51 -0400

    Added Toolbar. Working on ActionTab next.

--- app/src/main/java/com/nyu/mouzhang/ratemyfashiondemo/MainActivity.java
+++ app/src/main/java/com/nyu/mouzhang/ratemyfashiondemo/MainActivity.java
@@ -1,15 +1,32 @@
 package com.nyu.mouzhang.ratemyfashiondemo;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity{
+    final static String USERNAME = "admin";
+    final static String PASSWORD = "";
+    final static String RTSP_URL = "rtsp://192.168.1.185/play1.sdp";
+
+    private MediaPlayer mediaPlayer;
+    private SurfaceHolder surfaceHolder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +35,54 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        SurfaceView surfaceView = (SurfaceView)findViewById(R.id.surfaceview);
+//        surfaceHolder = surfaceView.getHolder();
+//        surfaceHolder.addCallback(this);
+//        surfaceHolder.setFixedSize(320, 240);
+
     }
+//    @Override
+//    public void surfaceChanged(SurfaceHolder sh, int f, int w, int h){}
+//    @Override
+//    public void surfaceCreated(SurfaceHolder sh){
+//        mediaPlayer = new MediaPlayer();
+//        mediaPlayer.setDisplay(surfaceHolder);
+//
+//        Context context = getApplicationContext();
+//        Map<String, String> headers = getRtspHeaders();
+//        Uri source = Uri.parse(RTSP_URL);
+//        try{
+//            mediaPlayer.setDataSource(context, source, headers);
+//            mediaPlayer.setOnPreparedListener(this);
+//            mediaPlayer.prepareAsync();
+//        }catch(Exception e){
+//
+//        }
+//    }
+//
+//    @Override
+//    public void onPrepared(MediaPlayer mp){
+//        mediaPlayer.start();
+//    }
+//    @Override
+//    public void surfaceDestroyed(SurfaceHolder sh){
+//        mediaPlayer.release();
+//    }
+//
+//    private Map<String, String> getRtspHeaders() {
+//        Map<String, String> headers = new HashMap<String, String>();
+//        String basicAuthValue = getBasicAuthValue(USERNAME, PASSWORD);
+//        headers.put("Authorization", basicAuthValue);
+//        return headers;
+//    }
+//
+//    private String getBasicAuthValue(String usr, String pwd) {
+//        String credentials = usr + ":" + pwd;
+//        int flags = Base64.URL_SAFE | Base64.NO_WRAP;
+//        byte[] bytes = credentials.getBytes();
+//        return "Basic " + Base64.encodeToString(bytes, flags);
+//    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,9 +99,6 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
